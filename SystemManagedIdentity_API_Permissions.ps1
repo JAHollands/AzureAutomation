dfd33c5182ab69eb060e1559ff591867ac3a798d
JAHollands <J.Hollands@Outlook.com>
2023-07-26 00:09:20 +0100

    Add files via upload

---
+++ SystemManagedIdentity_API_Permissions.ps1
@@ -0,0 +1,16 @@
+﻿Install-Module Microsoft.Graph -Scope CurrentUser
+
+Connect-MgGraph -Scopes Application.Read.All, AppRoleAssignment.ReadWrite.All, RoleManagement.ReadWrite.Directory
+
+#Object ID of the managed identity
+$managedIdentityId = "83409641-5384-4b7e-be0a-14ef228e61e0"
+#Role to grant
+$roleName = "User.ReadWrite.All"
+#app id bi = 00000009-0000-0000-c000-000000000000
+#app id graph = 00000003-0000-0000-c000-000000000000
+$msgraph = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
+$role = $Msgraph.AppRoles| Where-Object {$_.Value -eq $roleName} 
+
+New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $managedIdentityId -PrincipalId $managedIdentityId -ResourceId $msgraph.Id -AppRoleId $role.Id
+ 
+Disconnect-MgGraph
